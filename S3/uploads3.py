58dd9362f2275b9db52d532372dbee4df4a63ff7
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-12-06 19:52:04 -0300

    cloudformation

--- S3/uploads3.py
+++ S3/uploads3.py
@@ -7,7 +7,5 @@ s3 = session.resource('s3')
 filename = 'index.html'
 bucket_name = 'my-bucket13131lab12'
 
-# Uploads the given file using a managed uploader, which will split up large
-# files automatically and upload parts in parallel.
 
     
